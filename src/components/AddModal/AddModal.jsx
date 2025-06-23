d4beaefe2ec4e819e5be593036d332f821af57b3
Cmhanz99 <hanzhmagbal@gmail.com>
2025-06-23 10:03:22 +0800

    Commit

--- src/components/AddModal/AddModal.jsx
+++ src/components/AddModal/AddModal.jsx
@@ -3,7 +3,7 @@ import Inputs from "./Inputs";
 
 const AddModal = ({box, setBox, setOpenModal}) => {
   return (
-    <div className="fixed w-80 h-auto bg-slate-700 rounded-xl p-4 flex flex-col">
+    <div className="fixed inset w-80 h-auto bg-slate-700 rounded-xl p-4 flex flex-col">
       <h2 className="text-xl text-white font-semibold">Add Notes</h2>
       <Inputs box={box} setBox={setBox} setOpenModal={setOpenModal}/>
     </div>
